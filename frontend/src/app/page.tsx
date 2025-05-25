cf9bc1d0f33d246b0034f9b75cfc2dbcbe570a65
lakshyaa-k-nathan <lkn4@illinois.edu>
2025-05-25 12:06:09 -0500

    Tweaked up the search bar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import ClassSearch from "./components/ProfessorSearch";
 export default function Home() {
   return (
     <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Campus Analytics</h1>
+      <h1 className="text-2xl font-bold mb-4">Campus Dashboard</h1>
       <ClassSearch />
     </main>
   );
